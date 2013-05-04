cd361f132ec49f1f4d6d36436c298ed2ff853b65
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-05-04 17:29:42 +1200

    Revert "added slick models"
    This reverts commit 47dfa1224d88c7b851ed195a90a72c6d94b99ac9.

--- app/password/SPassword.scala
+++
@@ -1,5 +0,0 @@
-package password
-
-class SPassword {
-
-}
--- project/Build.scala
+++ project/Build.scala
@@ -10,14 +10,11 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm,
-    "com.typesafe" % "slick_2.10.0-M7" % "0.11.1",
-    "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
-    "org.scala-lang" % "scala-actors" % "2.10.0"    % "test"
+    anorm
   )
 
+
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    testOptions in Test := Nil
     // Add your own project settings here      
   )
 
