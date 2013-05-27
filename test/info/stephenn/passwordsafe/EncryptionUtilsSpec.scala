3882fd6dd2b196e4b76d4018ec3b27b7efaa28ff
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-05-27 22:06:24 +1200

    encryption utils, get the passphrase from the properties

--- test/info/stephenn/passwordsafe/EncryptionUtilsSpec.scala
+++ test/info/stephenn/passwordsafe/EncryptionUtilsSpec.scala
@@ -6,13 +6,17 @@ import play.api.test._
 import play.api.test.Helpers._
 
 class EncryptionUtilsSpec extends FlatSpec with ShouldMatchers {
-  
+
+  val passwordConfig = Map("app.encryption.passphrase" -> "ILoveWork2013")
+
   "encryptor" should "hide the string" in {
-    
-    val x= EncryptionUtils.encrypt("Password10")
-    x should not equal("Password10")
-    x should equal("5JGZXs2xv6zx9BktxbcYow==")
-    
-    EncryptionUtils.decode(x) should equal("Password10")
+    running(FakeApplication(additionalConfiguration = passwordConfig)) {
+
+      val x = EncryptionUtils.encrypt("Password10")
+      x should not equal ("Password10")
+      x should equal("5JGZXs2xv6zx9BktxbcYow==")
+
+      EncryptionUtils.decode(x) should equal("Password10")
+    }
   }
 }
