5d3e061f624d11ad85a80b62ccc270b17eaa3182
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-06-11 20:53:20 +1200

    Renamed account.title -> account.name

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -14,7 +14,7 @@ create sequence s_account_id;
 
 create table account (
   id    bigint DEFAULT nextval('s_account_id') primary key,
-  title  varchar(256),
+  name  varchar(256),
   password varchar(256),
   description varchar(4000),
   hidden boolean,
